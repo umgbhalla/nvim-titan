b602abe6aec7339d83c2db972e1e1549cdd260bc
Umang Bhalla <umg.bhalla88@gmail.com>
2022-01-12 16:52:02 +0530

    feat(null-ls): updated null-ls hook with format on save

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -16,4 +16,14 @@ null_ls.setup({
 		formatting.stylua,
     -- diagnostics.flake8
 	},
+	on_attach = function(client)
+		if client.resolved_capabilities.document_formatting then
+			vim.cmd([[
+            augroup LspFormatting
+                autocmd! * <buffer>
+                autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()
+            augroup END
+            ]])
+		end
+	end,
 })
